4d9fec52f9d51aebe6e0b2f60e9a5155df433f00
jaisonjeba <jaiprabakar5@gmail.com>
2024-12-15 13:25:28 +0530

    final

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const postRoutes = require('./routes/posts');
+const postRoutes = require('./backend/routes/posts');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
